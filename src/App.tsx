6f12bf9963523b66e0e951913b66fbe956c01f80
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-29 20:31:14 +0300

    29102022

--- src/App.tsx
+++ src/App.tsx
@@ -25,4 +25,17 @@ function Rating() {
     )
 }
 
+function Accordion() {
+    return
+        <div>
+            <h3> Меню </h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+
+}
+
 export default App;
