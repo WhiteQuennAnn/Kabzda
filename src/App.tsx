3c3b0c08009620189bf96ddf89c22bbea9acd91c
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-29 20:24:53 +0300

    29102022

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
-
+// function declaration
 function App() {
+
+    // полезное что-то
+
+    // обязана вернуть JSX
   return (
     <div>
      This is App component
