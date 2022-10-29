9937276fbe148bdb636a041171ca2b92c8442905
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-29 20:28:37 +0300

    29102022

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
+
 // function declaration
 function App() {
-
     // полезное что-то
 
     // обязана вернуть JSX
-  return (
-    <div>
-     This is App component
-    </div>
-  );
+    return (
+        <div>
+            This is APP component
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+        </div>
+    )
 }
 
 export default App;
