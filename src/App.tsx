bf77fb0acd97f5fdb614141ab41d78b002ef1881
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-30 15:36:00 +0300

    30102022

--- src/App.tsx
+++ src/App.tsx
@@ -45,15 +45,26 @@ function Star() {
 function Accordion() {
     console.log("Accordion rendering")
        return (
-
         <div>
-            <h3> Меню </h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle />
+            <AccordionBody />
         </div>
+    )
+}
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return (
+        <h3> Меню </h3>
+    )
+}
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     )
 }
 
