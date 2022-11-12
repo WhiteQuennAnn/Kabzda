fff3bf773796aa28a43247d8d76a3c9601edb3a0
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-12 22:36:10 +0300

    12112022

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -8,12 +8,21 @@ type AccordionPropsType = {
 function Accordion(props: AccordionPropsType) {
     debugger
     console.log("Accordion rendering")
-    return (
-        <div>
-            <AccordionTitle title={props.titleValue}/>
-            <AccordionBody/>
-        </div>
-    )
+    if (props.collapsed === true) {
+        return (
+            <div>
+                <AccordionTitle title={props.titleValue}/>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <AccordionTitle title={props.titleValue}/>
+                <AccordionBody/>
+            </div>
+        )
+    }
+
 }
 
 type AccordionTitlePropsType = {
