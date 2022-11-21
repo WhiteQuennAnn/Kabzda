121f28ee9807a0aaa427bdb4a698dd80ef2614ef
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-21 19:44:50 +0300

    21112022

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
-import {OnOff} from "./components/OnOff/OnOff";
+import OnOff from "./components/OnOff/OnOff";
+
 
 
 // function declaration
@@ -13,15 +14,15 @@ function App(props: any) {
     return (
 
         <div>
-            <OnOff/>
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <OnOff on={true}/>
+            {/*<Accordion titleValue={"Menu"} collapsed={true}/>*/}
+            {/*<Accordion titleValue={"Users"} collapsed={false}/>*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
 
         </div>
     );
--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -2,24 +2,41 @@ import React from 'react';
 
 type PropsType = {
     on: boolean
-    off: boolean
+    // off: boolean
 }
 
-export const OnOff = (props: PropsType) => {
-const onStyle={}
-const offStyle={}
-const indicatorStyle={
-    widht: "30px",
-    height: "30px",
-    borderRadius: "15px",
-    border: "1px solid black"
-
-}
+function OnOff(props: PropsType) {
+    const onStyle = {
+        widht: "50px",
+        height: "40px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px"
+    }
+    const offStyle = {
+        widht: "50px",
+        height: "40px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "2px",
+        padding: "2px"
+    }
+    const indicatorStyle = {
+        widht: "50px",
+        height: "50px",
+        borderRadius: "5px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "5px",
+        padding: "2px"
+    }
     return (
         <div>
-            <div></div>
-            <div></div>
+            <div style={onStyle}></div>
+            <div style={offStyle}></div>
             <div style={indicatorStyle}></div>
         </div>
     )
 }
+
+export default OnOff;
