a37d36612ab680d52b064e31e7d0c8d10a0651c9
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-21 19:28:09 +0300

    21112022

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
-export const OnOff= () => {
+type PropsType = {
+    on: boolean
+    off: boolean
+}
 
+export const OnOff = (props: PropsType) => {
+const onStyle={}
+const offStyle={}
+const indicatorStyle={
+    widht: "30px",
+    height: "30px",
+    borderRadius: "15px",
+    border: "1px solid black"
+
+}
     return (
         <div>
-
+            <div></div>
+            <div></div>
+            <div style={indicatorStyle}></div>
         </div>
     )
 }
