f2582ddf44e51558408e1b6613dced2752427da4
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-21 21:01:29 +0300

    21112022

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -2,16 +2,21 @@ import React, {useState} from "react";
 
 type RatingPropsType = {}
 
-export function UnControlledRating (props: RatingPropsType) {
+export function UnControlledRating(props: RatingPropsType) {
     console.log("UnControlledRating rendering")
     let [value, setValue] = useState(1)
     return (
         <div>
-            <Star selected={value > 0}/> <button> 1</button>
-            <Star selected={value > 1}/><button> 2</button>
-            <Star selected={value > 2}/><button> 3</button>
-            <Star selected={value > 3}/><button> 4</button>
-            <Star selected={value > 4}/><button> 5</button>
+            <Star selected={value > 0}/>
+            <button onClick={() => { setValue(1)}}> 1</button>
+            <Star selected={value > 1}/>
+            <button onClick={() => { setValue(2)}}> 2</button>
+            <Star selected={value > 2}/>
+            <button onClick={() => { setValue(3)}}> 3</button>
+            <Star selected={value > 3}/>
+            <button onClick={() => { setValue(4)}}> 4</button>
+            <Star selected={value > 4}/>
+            <button onClick={() => { setValue(5)}}> 5</button>
         </div>
     )
 }
