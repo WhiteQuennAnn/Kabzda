688b1f4cd462fc46735acf23292e62bc3af01d76
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-30 10:16:53 +0300

    30112022

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -8,24 +8,26 @@ export function UnControlledRating(props: RatingPropsType) {
     let [value, setValue] = useState(1)
     return (
         <div>
-            <Star selected={value > 0}/>
-            <Star selected={value > 1}/>
-            <Star selected={value > 2}/>
-            <Star selected={value > 3}/>
-            <Star selected={value > 4}/>
+            <Star selected={value > 0} setValue={setValue}/>
+            <Star selected={value > 1} setValue={setValue}/>
+            <Star selected={value > 2} setValue={setValue}/>
+            <Star selected={value > 3} setValue={setValue}/>
+            <Star selected={value > 4} setValue={setValue}/>
         </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
+    setValue: (value: 1|2|3|4|5)=> void
 }
 
 function Star(props: StarPropsType) {
     console.log("Star rendering")
-    return props.selected ? <span> <b> star </b></span> : <span>star </span>
 
-    return <span> {props.selected ? <b> star </b> : "star "} </span>
+    return <span>
+        {props.selected ? <b> star </b> : "star "}
+    </span>
 
 }
 
