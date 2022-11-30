0f033cba9f3a5036e080e658f82263ef24016cf3
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-30 10:12:18 +0300

    30112022

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -4,19 +4,35 @@ type RatingPropsType = {}
 
 export function UnControlledRating(props: RatingPropsType) {
     console.log("UnControlledRating rendering")
+
     let [value, setValue] = useState(1)
     return (
         <div>
             <Star selected={value > 0}/>
-            <button onClick={() => { setValue(1)}}> 1</button>
+            <button onClick={() => {
+                setValue(1)
+            }}> 1
+            </button>
             <Star selected={value > 1}/>
-            <button onClick={() => { setValue(2)}}> 2</button>
+            <button onClick={() => {
+                setValue(2)
+            }}> 2
+            </button>
             <Star selected={value > 2}/>
-            <button onClick={() => { setValue(3)}}> 3</button>
+            <button onClick={() => {
+                setValue(3)
+            }}> 3
+            </button>
             <Star selected={value > 3}/>
-            <button onClick={() => { setValue(4)}}> 4</button>
+            <button onClick={() => {
+                setValue(4)
+            }}> 4
+            </button>
             <Star selected={value > 4}/>
-            <button onClick={() => { setValue(5)}}> 5</button>
+            <button onClick={() => {
+                setValue(5)
+            }}> 5
+            </button>
         </div>
     )
 }
@@ -27,12 +43,8 @@ type StarPropsType = {
 
 function Star(props: StarPropsType) {
     console.log("Star rendering")
-    if (props.selected === true) {
-        return <span> <b>star </b></span>
-    } else {
-        return <span>star </span>
+    return props.selected ? <span> <b> star </b></span> : <span>star </span>
 
-    }
 }
 
 
