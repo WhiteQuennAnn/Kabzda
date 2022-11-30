7c1e35b5c493c64919499fb6aa1de64cfd9162f3
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-30 10:14:18 +0300

    30112022

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -9,30 +9,10 @@ export function UnControlledRating(props: RatingPropsType) {
     return (
         <div>
             <Star selected={value > 0}/>
-            <button onClick={() => {
-                setValue(1)
-            }}> 1
-            </button>
             <Star selected={value > 1}/>
-            <button onClick={() => {
-                setValue(2)
-            }}> 2
-            </button>
             <Star selected={value > 2}/>
-            <button onClick={() => {
-                setValue(3)
-            }}> 3
-            </button>
             <Star selected={value > 3}/>
-            <button onClick={() => {
-                setValue(4)
-            }}> 4
-            </button>
             <Star selected={value > 4}/>
-            <button onClick={() => {
-                setValue(5)
-            }}> 5
-            </button>
         </div>
     )
 }
@@ -44,6 +24,8 @@ type StarPropsType = {
 function Star(props: StarPropsType) {
     console.log("Star rendering")
     return props.selected ? <span> <b> star </b></span> : <span>star </span>
+
+    return <span> {props.selected ? <b> star </b> : "star "} </span>
 
 }
 
