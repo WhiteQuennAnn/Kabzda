36156c4911d41792983ac808eba7982aa2f4ae4f
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-30 10:43:29 +0300

    30112022

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,33 +1,35 @@
 import React from "react";
 
-export type RatingValueType = 0|1|2|3|4|5
+export type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5
 
 type RatingPropsType = {
     value: RatingValueType
-    onClick: (value: RatingValueType)=>void
+    onClick: (value: RatingValueType) => void
 }
 
 export function Rating(props: RatingPropsType) {
     console.log("UnControlledRating rendering")
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
+    onClick: (value: RatingValueType) => void
+    value: RatingValueType
 }
 
 function Star(props: StarPropsType) {
     console.log("Star rendering")
 
-        return <span>
+    return <span>
        {props.selected ? <b> star </b> : " star "}
     </span>
 
