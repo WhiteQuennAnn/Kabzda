c4ccd07783765204d3ccf4b4601b6b603a5e9eaf
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-14 20:29:18 +0300

    14122022

--- src/components/Input/UncontrolledInput.tsx
+++ src/components/Input/UncontrolledInput.tsx
@@ -8,8 +8,12 @@ import React, {useState} from 'react';
 export const UncontrolledInput = () => <input/>;
 
 export const TrackValueOfUncontrolledInput = () => {
-
-
+    const [value, setValue] = useState("");
+    return <> <input onChange={(event) => {
+        const actualValue = event.currentTarget.value;
+        setValue(actualValue)
+    }}/>
+    </>
 }
 
 export const ControlledInput = () => <input value={"Anna"}/>;
