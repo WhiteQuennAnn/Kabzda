9981b3880613b877feef4dcc8fef43b9e92d3803
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-26 19:05:31 +0300

    26122022

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -14,8 +14,11 @@ type SelectPropsType = {
 
 export function Select(props: SelectPropsType) {
     const [active, setActive] = useState(false);
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value);
 
     const selectedItem = props.items.find(i => i.value === props.value)
+    const hoveredItem = props.items.find(i => i.value === hoveredElementValue)
+
     const toggleItems = () => {
         setActive(!active)
     }
@@ -38,12 +41,16 @@ export function Select(props: SelectPropsType) {
                     active &&
 
                     <div className={styles.items}>
-                        {props.items.map(i => <div className={styles.item + " " +
-                            (selectedItem === i ? styles.selected : " ")}
-                                                   key={i.value}
-                                                   onClick={() => {
-                                                       onItemClick(i.value)
-                                                   }}>
+                        {props.items.map(i => <div
+                            onMouseEnter={() => {
+                                setHoveredElementValue(i.value)
+                            }}
+                            className={styles.item + " " +
+                                (selectedItem === i ? styles.selected : " ")}
+                            key={i.value}
+                            onClick={() => {
+                                onItemClick(i.value)
+                            }}>
                             {i.title}
                         </div>)}
                     </div>
