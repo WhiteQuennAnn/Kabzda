142f9d01f22aa0c69a3c12ea4f349ae16f74af0e
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-07 18:17:25 +0300

    lesson 16 useReducer

--- src/components/AccordionUnControlled/Reducer.test.tsx
+++ src/components/AccordionUnControlled/Reducer.test.tsx
@@ -12,3 +12,31 @@ test('collapsed should be true', () => {
     //expect
     expect(newState.collapsed).toBe(true)
 })
+
+
+test('collapsed should be false', () => {
+
+    //data
+    const state: StateType = {
+        collapsed: true
+    }
+    //action
+    const newState = reducer(state, {type: TOGGLE_COLLAPSED})
+
+    //expect
+    expect(newState.collapsed).toBe(false)
+})
+
+test('reducer should throw error because action type is incorrect', () => {
+
+    //data
+    const state: StateType = {
+        collapsed: true
+    }
+    //action
+    expect(() => {
+        reducer(state, {type: "FAKE"})
+    }).toThrowError()
+
+
+})
