e9267027086fc45465778a14c911670108640db2
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-08 20:19:42 +0300

    lesson 17 React.Memo

--- src/components/ReactMemo.tsx
+++ src/components/ReactMemo.tsx
@@ -1,3 +1,15 @@
 export default {
+    title: "React memo demo",
+}
+
+
+const Filter = (props: any) => {
+    return <div> {props.count}</div>
+}
+
+const Table = (props: any) => {
+    return <div> {props.count}</div>
+}
+const Example = () => {
 
 }
