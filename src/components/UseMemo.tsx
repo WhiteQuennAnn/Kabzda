dbc325a58a568634084fea40d5f6d959dd3b3a28
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-08 21:13:08 +0300

    lesson 18 UseMemo

--- src/components/UseMemo.tsx
+++ src/components/UseMemo.tsx
@@ -2,11 +2,18 @@ import React, {useState} from "react";
 
 export const Example1 = () => {
 
-    const [a, setA] = useState<number>(0)
-    const [b, setB] = useState<number>(0)
+    const [a, setA] = useState<number>(3)
+    const [b, setB] = useState<number>(3)
 
     return <>
         <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
         <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/> //можно и так, и так
+        <hr/>
+        <div>
+            Result for a: {}
+        </div>
+        <div>
+            Result for b: {}
+        </div>
     </>
 }
